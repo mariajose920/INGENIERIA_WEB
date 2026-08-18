06b164f6e2869b1a877db7af56eb9c14b7ce0a3d
mariajose920 <mj.vilchesaranguiz@gmail.com>
2026-08-18 12:45:44 -0400

    Actividad 4 - Formulario con validación

--- PROYECTOWEB1/src/App.vue
+++ PROYECTOWEB1/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Padre from './components/Padre.vue'
+import Contacto from './components/Contacto.vue'
 </script>
 
 <template>
-  <Padre />
+  <div>
+    <Padre />
+    <Contacto />
+  </div>
 </template>
