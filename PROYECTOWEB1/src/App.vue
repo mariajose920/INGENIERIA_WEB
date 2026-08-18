f3fde0ecdb28bca07d34064b523ef2b826746d79
mariajose920 <mj.vilchesaranguiz@gmail.com>
2026-08-18 11:56:41 -0400

    guia  2 terminada

--- PROYECTOWEB1/src/App.vue
+++ PROYECTOWEB1/src/App.vue
@@ -1,15 +1,7 @@
-<template>
-  <div id="app">
-    <Padre />
-  </div>
-</template>
-
-<script>
+<script setup>
 import Padre from './components/Padre.vue'
-
-export default {
-  components: {
-    Padre
-  }
-}
 </script>
+
+<template>
+  <Padre />
+</template>
